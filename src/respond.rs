366e25d62257e1b500493d81d40cfdf3574d2974
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2025-05-07 17:07:06 -0700

    vibecoded?

--- src/respond.rs
+++ src/respond.rs
@@ -2,6 +2,6 @@ use crate::util::{extract_paths, generate_challenge_hash};
 
 pub fn generate_response(received_response: String) -> String {
     let paths = extract_paths(&received_response);
-    let hashes = paths.iter().map(|path| generate_challenge_hash(path) ).collect::<Result<Vec<_>, _>>();
-    hashes.unwrap().join(";")
+    let hashes: Result<Vec<String>, anyhow::Error> = paths.iter().map(|path| generate_challenge_hash(path) ).collect::<Result<Vec<_>, _>>();
+    hashes.unwrap_or_default().join(";")
 }
--- src/util.rs
+++ src/util.rs
@@ -10,12 +10,8 @@ pub fn extract_paths(joined_paths: &String) -> Vec<&str> {
 
 fn get_first_line(path: &str) -> Result<String> {
     // try opening claim file
+    let file: File = File::open(path)?;
 
-    // if metadata(path).unwrap().is_dir() {
-    //     return Ok("".to_string());
-    // }
-
-    let file: File = File::open(path).with_context(|| format!("Failed to open file {}", path)).expect("");
     let mut reader: BufReader<File> = BufReader::new(file);
     let mut first_line = String::new();
 
